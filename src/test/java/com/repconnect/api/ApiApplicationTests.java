8ddb52e2e6400bf924b1fc854b00956c5e1310d7
Marcio Souza <marciooliveira10@gmail.com>
2023-12-18 06:48:39 +0000

    first commit

---
+++ src/test/java/com/repconnect/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repconnect.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
